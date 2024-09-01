bd307b868885aba7c8b9b3917c4f788857692700
ugbeadie <ugbeadie3@gmail.com>
2024-09-01 20:39:45 +0100

    initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWrgVLQmZMUqq-qy6shd2ixlttUTmBdSM",
+  authDomain: "quickbooks-15072.firebaseapp.com",
+  projectId: "quickbooks-15072",
+  storageBucket: "quickbooks-15072.appspot.com",
+  messagingSenderId: "495200749252",
+  appId: "1:495200749252:web:1f679c7b52d44603e32b91",
+  measurementId: "G-HKF9W129C2",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
