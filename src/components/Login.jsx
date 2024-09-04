25785bff72b60386702cf85b03d327dcf07c5143
ugbeadie <ugbeadie3@gmail.com>
2024-09-04 21:56:50 +0100

    style login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,12 +2,15 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../config/firebase";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const navigate = useNavigate();
 
@@ -36,7 +39,75 @@ const Login = () => {
   };
   return (
     <>
-      <div className="w-[30%]">
+      <section className="w-[50%] mx-auto flex flex-col gap-10 text-center">
+        <div className="title">
+          <h1 className="text-gray-800 text-4xl font-bold py-4">Login</h1>
+          <p className="w-3/4 mx-auto text-gray-400">
+            Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+        <form className="flex flex-col gap-5">
+          <div className="flex border rounded-xl relative">
+            <input
+              className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border:none"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="flex border rounded-xl relative">
+            <input
+              className="w-full py-4 px-6 border rounded-xl bg-slate-50 focus:outline-none border:none"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+            />
+            <span
+              className="icon flex items-center px-4 absolute top-[17px] right-[5px] cursor-pointer"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? (
+                <IoEyeOutline
+                  size={25}
+                  className="text-[#2d3e53] hover:text-[#6366f1]"
+                />
+              ) : (
+                <IoEyeOffOutline
+                  size={25}
+                  className="text-[#2d3e53] hover:text-[#6366f1]"
+                />
+              )}
+            </span>
+          </div>
+          <div className="input-btn">
+            <button
+              className="w-full bg-gradient-to-r from-blue-500 to-indigo-500 rounded py-3 text-gray-50 text-lg "
+              type="submit"
+            >
+              Login
+            </button>
+            <div>
+              <button className="w-full border py-3 flex justify-center items-center gap-2 hover:bg-gray-200">
+                Sign in with Google <FcGoogle size={25} />
+              </button>
+            </div>
+          </div>
+        </form>
+        <p className="text-center text-gray-400">
+          Don't have an account?{" "}
+          <Link className="text-blue-700" to="/signup">
+            sign up
+          </Link>
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Login;
+
+{
+  /* <div className="w-[30%]">
         LOGIN
         <form onSubmit={handleSubmit} className="flex flex-col ">
           {error && <p className="text-red-600">{error}</p>}
@@ -64,9 +135,5 @@ const Login = () => {
           {" "}
           dont have an account? <Link to="/signup">sign up</Link>
         </div>
-      </div>
-    </>
-  );
-};
-
-export default Login;
+      </div> */
+}
