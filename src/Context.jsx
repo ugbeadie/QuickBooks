efe61df9bf37d14c49252491c38fbb4aa966c280
ugbeadie <ugbeadie3@gmail.com>
2024-09-10 17:05:01 +0100

    improve folder structure

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,15 +1,12 @@
 import { createContext, useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./config/firebase";
-import { useGetTransactions } from "../hooks/useGetTransactions";
 
 export const userAuthContext = createContext();
 
 const Context = ({ children }) => {
   const [user, setUser] = useState({});
   const [query, setQuery] = useState("");
-  const [items, setItems] = useState(products);
-  const { transactions, transactionValues } = useGetTransactions();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
